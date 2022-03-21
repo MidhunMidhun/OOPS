424f5e817a606e12fb39ec8d9b56729f2d5eaa4b
MidhunMidhun <93638347+MidhunMidhun@users.noreply.github.com>
2022-03-21 15:17:12 +0530

    q1

---
+++ Area.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Area
+{
+	public static void main(String args[])
+	{
+		int l,b,area;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the length");
+		l= s.nextInt();
+		System.out.println("Enter the breadth");
+		b= s.nextInt();
+		area=l*b;
+		System.out.println("The area is "+area);
+	}
+}
+		
